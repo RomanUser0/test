420de85e8946511ce0ebd02391fdea83e2bdc1df
roman <zroman938@gmail.com>
2024-03-14 23:07:54 +0300

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,17 @@ import { useForm } from 'react-hook-form'
 import axios from 'axios'
 import { useDispatch, useSelector } from 'react-redux'
 import { getProfile } from './store/slices/authSlice'
+import { useEffect } from 'react'
 
 function App() {
   const user = useSelector(state => state.authSlice.user)
   const dispatch = useDispatch()
+
+
+  useEffect(() => {
+    console.log(user)
+  },[user])
+
 
   const {
     register,
@@ -17,12 +24,12 @@ function App() {
   } = useForm()
 
   const onSubmit = async (data) => {
-    await axios.post('api/auth/login',{email: data.email, password: data.password}).then((result) => {
+    await axios.post('api/auth/login', { email: data.email, password: data.password }).then((result) => {
       dispatch(getProfile(result.data))
-      console.log(user)
+      
     })
   }
-  
+
 
 
 
