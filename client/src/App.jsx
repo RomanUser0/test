bbd73b6b978c37c9b442c592565912ffa5a99c4e
roman <zroman938@gmail.com>
2024-03-14 22:56:39 +0300

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
 
   const onSubmit = async (data) => {
     await axios.post('api/auth/login',{email: data.email, password: data.password}).then((result) => {
-      dispatch(getProfile(JSON.parse(result)))
+      dispatch(getProfile(result))
     })
   }
 
