9cd0d822e9ae7026bcd6ab44195aef14ebc6df45
roman <zroman938@gmail.com>
2024-03-14 22:01:46 +0300

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,12 @@
 import './App.css'
 import { useForm } from 'react-hook-form'
 import axios from 'axios'
+import { useDispatch, useSelector, useSelector } from 'react-redux'
+import { getProfile } from './store/slices/authSlice'
 
 function App() {
+  const user = useSelector(state => state.authSlice.user)
+  const dispatch = useDispatch()
 
   const {
     register,
@@ -13,8 +17,8 @@ function App() {
   } = useForm()
 
   const onSubmit = async (data) => {
-   await axios.post('api/auth/login', {...data}).then((res, req) => {
-      console.log(res)
+    await axios.post('api/auth/login', data).then((res, req) => {
+      dispatch(getProfile(data.data))
     })
   }
 
@@ -23,13 +27,13 @@ function App() {
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)} className='button_form'>
-        <input type='text' placeholder='Name' {...register('name')}/>
-        <input type='password' placeholder='Password' {...register('password')}/>
-        <input type='email' placeholder='Email'{...register('email')}/>
+        <input type='text' placeholder='Name' {...register('name')} />
+        <input type='password' placeholder='Password' {...register('password')} />
+        <input type='email' placeholder='Email'{...register('email')} />
         <button>Отправить</button>
       </form>
       <div>
-        
+
       </div>
     </>
   )
---
+++ client/src/store/slices/authSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    user: {
+        name: '',
+        email: '',
+        password: ''
+    },
+    isAuth: false
+}
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    redusers: {
+        getProfile(state, action) {
+            state.user === action
+            state.isAuth = true
+        }
+    }
+})
+
+export const { getProfile } = authSlice.actions
+export default authSlice.reducer
