c5e1368fb90e2f419ff4d95c8504399d83db414b
roman <zroman938@gmail.com>
2024-03-15 16:37:31 +0300

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -32,7 +32,7 @@ function App() {
 
   return (
     <>
-      <form onSubmit={handleSubmit(onSubmit)} className='button_form'>
+      <form onSubmit={handleSubmit(onSubmit)} className='button_form'>   
         <input type='text' placeholder='Name' {...register('name')} />
         <input type='password' placeholder='Password' {...register('password')} />
         <input type='email' placeholder='Email'{...register('email')} />
