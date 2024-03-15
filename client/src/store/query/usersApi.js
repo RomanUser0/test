7a818dad13b96fae4b291070ed4504458383bd27
roman <zroman938@gmail.com>
2024-03-15 17:41:59 +0300

    .

--- client/src/store/query/usersApi.js
+++ client/src/store/query/usersApi.js
@@ -4,19 +4,27 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const usersApi = createApi({
     reducerPath: 'getUsers',
+    tagTypes: ['Users'],
     baseQuery: fetchBaseQuery({ baseUrl: 'api' }),
     endpoints: (build) => ({
         getUsers: build.query({
             query: () => 'users',
+            providesTags: (result) => result
+                ? [
+                    ...result.map(({ id }) => ({ type: 'Users', id })),
+                    { type: 'Users', id: 'USER' },
+                ]
+                : [{ type: 'Users', id: 'USER' }]
         }),
         createUser: build.mutation({
             query: (body) => ({
                 url: 'create',
                 method: 'POST',
                 body
-            })
-        }) 
+            }),
+            invalidatesTags: [{ type: 'Users', id: 'USER' }]
+        })
     })
 })
-  
+
 export const { useGetUsersQuery, useCreateUserMutation } = usersApi
