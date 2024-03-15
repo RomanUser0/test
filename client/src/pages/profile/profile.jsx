ad3d7f7de97bdeb2ce2f46c79f8fb74cf9f03118
roman <zroman938@gmail.com>
2024-03-15 19:39:44 +0300

    .

--- client/src/pages/profile/profile.jsx
+++ client/src/pages/profile/profile.jsx
@@ -1,12 +1,26 @@
+import { useGetUsersQuery } from "../../store/query/usersApi"
 
 
 
 
 
 function Profile() {
+
+    const { users, isLoading}  = useGetUsersQuery()
+
+
+
     return(
         <div>
-            Profile
+            {
+                users.map(({id, name, email}) => {
+                    <div>
+                        <span>{id}</span>
+                        <span>{name}</span>
+                        <span>{email}</span>
+                    </div>
+                })
+            }
         </div>
     )
 }
