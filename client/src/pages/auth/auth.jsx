89d3614f91d520c5b4ce1dbcfc2cc4b4aa1a5bf4
roman <zroman938@gmail.com>
2024-03-15 19:21:59 +0300

    .

--- client/src/pages/auth/auth.jsx
+++ client/src/pages/auth/auth.jsx
@@ -11,7 +11,7 @@ function Auth() {
     const dispatch = useDispatch()
     const { data = [] } = useGetUsersQuery()
     const [createUser, { isLoading }] = useCreateUserMutation()
-    const [ getProfile ] = useCreateUserMutation()
+    const [getProfile] = useCreateUserMutation()
     const [isAuth, setIsAuth] = useState(false)
     console.log(isAuth)
 
@@ -19,33 +19,34 @@ function Auth() {
     const { register, formState: { errors }, handleSubmit } = useForm()
 
     const onSubmit = async (user) => {
-        if (isAuth === true) {
-            const data = await getProfile(user).unwrap()
-            dispatch(getProfile(data))
-        } else if (isAuth === false) {
-            const data = await createUser(user).unwrap()
-            dispatch(getProfile(data))
+        if (isAuth) {
+            const profile = await getProfile(user).unwrap()
+            dispatch(getProfile(profile))
         }
 
+        const data = await createUser(user).unwrap()
+        dispatch(getProfile(data))
     }
 
-    return (
-        <div>
-            <form onSubmit={handleSubmit(onSubmit)} className='button_form'>
-                <input type='text' placeholder='Name' {...register('name')} />
-                <input type='password' placeholder='Password' {...register('password')} />
-                <input type='email' placeholder='Email'{...register('email')} />
-                <button>Отправить</button>
-            </form>
-            {isAuth ?
-                <div onClick={() => setIsAuth(!isAuth)}>Вы хотите зарегестрироваться?</div>
-                :
-                <div onClick={() => setIsAuth(!isAuth)}>У вас есть аккаунт?</div>
-            }
+}
+
+return (
+    <div>
+        <form onSubmit={handleSubmit(onSubmit)} className='button_form'>
+            <input type='text' placeholder='Name' {...register('name')} />
+            <input type='password' placeholder='Password' {...register('password')} />
+            <input type='email' placeholder='Email'{...register('email')} />
+            <button>Отправить</button>
+        </form>
+        {isAuth ?
+            <div onClick={() => setIsAuth(!isAuth)}>Вы хотите зарегестрироваться?</div>
+            :
+            <div onClick={() => setIsAuth(!isAuth)}>У вас есть аккаунт?</div>
+        }
 
 
-        </div>
-    )
+    </div>
+)
 }
 
 export default Auth
